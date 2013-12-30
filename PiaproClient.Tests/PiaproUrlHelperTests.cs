09e5560bcb434a22153c46df9135157516f99ffc
Jukka <riipah@gmail.com>
2013-12-30 14:13:25 +0200

    Added PiaproUrlHelper

---
+++ PiaproClient.Tests/PiaproUrlHelperTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PiaproClient.Tests {
+
+	/// <summary>
+	/// Tests for <see cref="PiaproUrlHelper"/>.
+	/// </summary>
+	[TestClass]
+	public class PiaproUrlHelperTests {
+
+		private void TestIsValidContentUrl(string url, bool expected) {
+			Assert.AreEqual(PiaproUrlHelper.IsValidContentUrl(url), expected);
+		}
+
+		[TestMethod]
+		public void IsValidContentUrl_ShortUrl() {
+			
+			TestIsValidContentUrl("http://piapro.jp/t/abcd", true);
+
+		}
+
+		[TestMethod]
+		public void IsValidContentUrl_LongUrl() {
+			
+			TestIsValidContentUrl("http://piapro.jp/content/abcd1234efg", true);
+
+		}
+
+		[TestMethod]
+		public void IsValidContentUrl_NotValid() {
+			
+			TestIsValidContentUrl("http://piapro.jp/", false);
+
+		}
+
+	}
+}
---
+++ PiaproClient/PiaproUrlHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace PiaproClient {
+
+	/// <summary>
+	/// Helper methods for Piapro URLs.
+	/// </summary>
+	public static class PiaproUrlHelper {
+
+		public static readonly string[] PiaproUrlRegexes = {
+			@"piapro.jp/t/([\w\-]+)",		// Short URLs
+			@"piapro.jp/content/([\w\-]+)"	// Long URLs
+		};
+
+		/// <summary>
+		/// Tests whether an URL is a valid Piapro content URL.
+		/// </summary>
+		/// <param name="url">URL to be tested. Can be null or empty.</param>
+		/// <returns>True if the URL is valid, otherwise false.</returns>
+		public static bool IsValidContentUrl(string url) {
+
+			if (string.IsNullOrEmpty(url))
+				return false;
+
+			return PiaproUrlRegexes.Any(r => Regex.IsMatch(url, r, RegexOptions.IgnoreCase));
+
+		}
+
+	}
+
+}
